26a71800ad081f5070ce54e0cc139936f2f47430
Vitor <Vitor@192.168.1.158>
2018-05-02 21:49:25 +0100

    grda borla mpt

---
+++ VVS_assignment_1/src/test/java/sut/TestTSTKeys.java
@@ -0,0 +1,28 @@
+package sut;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Queue;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.Test;
+
+public class TestTSTKeys {
+
+	static TST<Integer> st;
+
+	@Test
+	public void testKeys() {
+		TST<Integer> tst = new TST<>();
+		tst.put("oi", 1);
+		tst.put("ok", 2);
+		assertEquals("[oi, ok]", tst.keys().toString());
+		
+	}
+	
+}
---
+++ VVS_assignment_1/src/test/java/sut/TestTSTKeysWithPrefix.java
@@ -0,0 +1,56 @@
+package sut;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TestTSTKeysWithPrefix {
+
+	static TST<Integer> st;
+
+	@BeforeAll
+	public static void setup() {
+		try {
+			Scanner sc = new Scanner(new File("data/someWords.txt"));
+			st = new TST<>();
+			int i=0;
+			while(sc.hasNextLine()) {
+				String[] keys = sc.nextLine().split(" ");
+				for(String key : keys)
+					st.put(key, ++i);
+			}
+			sc.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testNullArgument() {
+		String s = null;
+		assertThrows(IllegalArgumentException.class, () -> {st.keysWithPrefix(s);});
+	}
+	
+	@Test
+	public void testMissingPrefix() {
+		String s = "zzz";
+		assertEquals(new LinkedList<>(), st.keysWithPrefix(s));
+	}
+	
+	@Test
+	public void testRepeatedPrefix() {
+		String s = "she";
+		LinkedList<String> list = new LinkedList<String>();
+		list.add("she");
+		list.add("shells");
+		assertEquals(list, st.keysWithPrefix(s));
+	}
+	
+}
