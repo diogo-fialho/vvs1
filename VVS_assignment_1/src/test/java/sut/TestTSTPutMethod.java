6a1dc140ba46ad2264ec97223bff7dce161d1716
Vitor <Vitor@10.101.149.84>
2018-05-04 15:04:55 +0100

    syncing

--- VVS_assignment_1/src/test/java/sut/TestTSTPutMethod.java
+++ VVS_assignment_1/src/test/java/sut/TestTSTPutMethod.java
@@ -4,14 +4,25 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
+//further tested in contains
 public class TestTSTPutMethod {
-
 	
 	@Test
-	public void testAddElement() {
+	public void testAddNullKey() {
 		TST<Integer> st = new TST<>();
 		assertThrows(IllegalArgumentException.class, () -> {
 			st.put(null, 2);
 		});
 	}
+	
+	@Test
+	public void testRepeatedKey(){
+		
+		TST<Integer> st = new TST<Integer>();
+		String key = "pao";
+		st.put(key, 1);
+		st.put(key, 2);
+		
+	}
+	
 }
