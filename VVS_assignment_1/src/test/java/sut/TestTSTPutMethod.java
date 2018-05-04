2947b0cfda7befbdec76a373bdbf016f0ea3be32
Vitor <Vitor@10.101.149.84>
2018-05-04 15:34:36 +0100

    added put tests (wip)

--- VVS_assignment_1/src/test/java/sut/TestTSTPutMethod.java
+++ VVS_assignment_1/src/test/java/sut/TestTSTPutMethod.java
@@ -2,10 +2,21 @@ package sut;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 //further tested in contains
 public class TestTSTPutMethod {
+	
+	TST<Integer> st;
+	
+	@BeforeEach
+	public void setup() {
+		st = new TST<Integer>();
+		st.put("prefix", 1);
+		st.put("teste", 2);
+	}
 	
 	@Test
 	public void testAddNullKey() {
@@ -18,10 +29,42 @@ public class TestTSTPutMethod {
 	@Test
 	public void testRepeatedKey(){
 		
-		TST<Integer> st = new TST<Integer>();
-		String key = "pao";
-		st.put(key, 1);
-		st.put(key, 2);
+		String key = "prefix";
+		st.put(key, 3); //val diferente?
+		
+	}
+	
+	@Test
+	public void testRepeatedPrefix(){
+		
+		String key = "pre";
+		st.put(key, 4);
+		
+	}
+	
+	@Test
+	public void testLargestKey(){
+		
+		String key = "loooooooooooooooooooooongkey";
+		st.put(key, 5);
+		
+		
+	}
+	
+	@Test
+	public void testAvgtKey(){
+		
+		String key = "avgkey";
+		st.put(key, 6);
+		
+		
+	}
+	
+	@Test
+	public void testSmallest(){
+		
+		String key = "z";
+		st.put(key, 7);
 		
 	}
 	
